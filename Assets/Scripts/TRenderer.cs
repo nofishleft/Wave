be9aceda7c51b4412aeb3e41023be1f4ea339286
Rishaan Gupta <notanapachehelicopter@gmail.com>
2018-10-21 01:36:49 +1300

    Rotating Turret

--- Assets/Scripts/TRenderer.cs
+++ Assets/Scripts/TRenderer.cs
@@ -56,6 +56,9 @@ namespace nz.rishaan.DynamicCuboidTerrain
             desiredCamPos = player.obj.transform.position + new Vector3(-20, 20, -20);
             cam.position += (((1 - asympConst) * cam.position + asympConst * desiredCamPos) - cam.position) * camSpeed * Time.deltaTime;
             updateObstacles();
+            float loc = cuboids[(int)player.obj.transform.position.x, (int)player.obj.transform.position.z].position.y + cuboids[(int)player.obj.transform.position.x, (int)player.obj.transform.position.z].localScale.y / 2 + 1;
+            loc = Mathf.Max(0.9f*loc + 0.1f * player.obj.transform.position.y, loc);
+            player.obj.transform.position = new Vector3(player.obj.transform.position.x, loc, player.obj.transform.position.z);
         }
 
         void updateObstacles() {
@@ -85,51 +88,25 @@ namespace nz.rishaan.DynamicCuboidTerrain
         public Transform lrRot;
         public float turnSpeed = 90f;
         float cSpeed = 0f;
-        float maxSpeed = 20f;
+        float maxSpeed = 2f;
         public float boatLength = 1f;
+        public Transform boat;
+        public Transform pointer;
 
         void move2()
         {
             int dir = 0;
-            Vector3 v = horRot.eulerAngles;
+            Vector3 v = horRot.localEulerAngles;
             if (Input.GetKey(KeyCode.A))
             {
-                
+
                 --dir;
             }
             if (Input.GetKey(KeyCode.D))
             {
-                
+
                 ++dir;
             }
-            if (dir > 0)
-            {
-                if (v.y >= 180) v.y = Mathf.Min(v.y + (0.9f * (405f) + 0.1f * v.y - v.y) * turnSpeed * Time.deltaTime, 405);
-                else v.y = Mathf.Min(v.y + (0.9f * (45f) + 0.1f * v.y - v.y) * turnSpeed * Time.deltaTime, 45);
-            }
-            else if (dir < 0)
-            {
-                if (v.y > 180) v.y = Mathf.Max(v.y + (0.9f * (315f) + 0.1f * v.y - v.y) * turnSpeed * Time.deltaTime, 315);
-                else if (v.y >= 0) v.y = Mathf.Max(v.y + (0.9f * (-45f) + 0.1f * v.y - v.y) * turnSpeed * Time.deltaTime, -45);
-            }
-            else {
-                if (v.y < 90 && v.y > 0) v.y = Mathf.Max(0.1f * v.y * turnSpeed * Time.deltaTime, 0);
-                else if (v.y < 0) v.y = Mathf.Min(0.1f * v.y * turnSpeed * Time.deltaTime, 0);
-                else v.y = Mathf.Min(v.y + (0.9f * (360f) + 0.1f * v.y - v.y) * turnSpeed * Time.deltaTime, 0);
-            }
-            
-
-            //Debug.Log(v.y);
-
-            if (dir == 0)
-            {
-                
-            }
-            horRot.eulerAngles = v;
-
-            //if (v.y < 0) v.y = Mathf.Clamp(v.y * Time.deltaTime, -180, -90);
-            
-
 
             int forback = 0;
             if (Input.GetKey(KeyCode.W))
@@ -140,44 +117,14 @@ namespace nz.rishaan.DynamicCuboidTerrain
             {
                 --forback;
             }
-            if (forback != 0) {
-                if ((int)Mathf.Sign(cSpeed) != forback) forback *= 2;
-                cSpeed = Mathf.Clamp(cSpeed + (forback * 20f * Time.deltaTime), -maxSpeed, maxSpeed);
-                //Vector3 targetFor = (vertRot.rotation * Vector3.right) * cSpeed * Time.deltaTime;
-                //player.obj.transform.position += targetFor;
-            } else {
-                if (cSpeed > 0)
-                {
-                    cSpeed = Mathf.Clamp(cSpeed - (20f * Time.deltaTime), 0, maxSpeed);
-                }
-                else if (cSpeed < 0) {
-                    cSpeed = Mathf.Clamp(cSpeed + (20f * Time.deltaTime), -maxSpeed, 0);
-                }
-                /*
-                Vector3 targetFor = (vertRot.rotation * Vector3.right) * cSpeed * Time.deltaTime;
-                player.obj.transform.position += targetFor;*/
+            if (forback != 0 || dir != 0) {
+                horRot.localRotation = Quaternion.RotateTowards(horRot.localRotation, Quaternion.LookRotation(new Vector3(dir, 0f, forback)), 500f* Time.deltaTime);
+                player.obj.transform.position += (pointer.position - boat.position).normalized * maxSpeed;
             }
-            player.obj.transform.position += new Vector3(cSpeed * Time.deltaTime, 0, -dir * (0.5f*Mathf.Abs(cSpeed) + 15f) * Time.deltaTime);
+            //new Vector3(forback, 0f, -dir).normalized
 
         }
-
-        void move(Vector3 dir)
-        {
-            dir = player.gloPos + dir;
-            dir.x = Mathf.Clamp(dir.x, 0f, renderRange - 1);
-            dir.z = Mathf.Clamp(dir.z, 0f, renderRange - 1);
-            player.gloPos = dir;
-            Debug.Log(dir.x);
-
-
-            /*
-            player.position += dir;
-            player.x = Mathf.Clamp(player.x, renderRange, mapSize - renderRange);
-            player.z = Mathf.Clamp(player.z, renderRange, mapSize - renderRange);
-            player.height = map.heightMap[(int)player.x, (int)player.z];
-            */
-        }
-
+    
         void add(Vector3 v)
         {
             int i = lastdirection.IndexOf(v);
@@ -273,7 +220,7 @@ namespace nz.rishaan.DynamicCuboidTerrain
             //p.AddComponent<Material>();
             updateHeights();
             int loc = renderRange / 2;
-            player.obj.transform.position = new Vector3(loc, cuboids[loc, loc].position.y + 10, loc);
+            player.obj.transform.position = new Vector3(loc, cuboids[loc, loc].position.y + 1, loc);
         }
 
         /*Spherical render range
@@ -307,7 +254,7 @@ namespace nz.rishaan.DynamicCuboidTerrain
                     //Debug.Log("y: " + map.heightMap[xShift + x,zShift + z]);
                     //float temp = Mathf.Abs(x + z - player.x - player.z);
                     //if (temp < 10) {
-                    float relHeight = map.heightMap[-X / 2 + x + (int)(player.x), -Z / 2 + z + (int)player.z] - 0.5f;
+                    float relHeight = (map.heightMap[-X / 2 + x + (int)(player.x), -Z / 2 + z + (int)player.z] - 0.5f)/2;
                     //relHeight += Mathf.Clamp(temp,0,10);
                     float scale = Mathf.Max(relHeight - map.minHeight, minBlockSize);
                     relHeight = relHeight - 0.5f * scale;
--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -9,15 +9,28 @@ public class Turret : MonoBehaviour {
     public GameObject barrelEnd;
     public Camera cam;
 
-    public float barrelRotation {
+    public float barrelRotationVert {
         get {
-            return
+            return barrel.transform.localRotation.eulerAngles.x;
+        }
+        set {
+            barrel.transform.localRotation = Quaternion.Euler(value,0,0);
+        }
+    }
+
+    public float barrelRotationHor
+    {
+        get
+        {
+            return transform.rotation.eulerAngles.y;
+        }
+        set
+        {
+            transform.rotation = Quaternion.Euler(0, value, 0);
         }
     }
 
     public GameObject shellPrefab;
-    int currentAmmo = 0;
-    Modifier[/*Ammo*/][/*Modifiers*/] modList;
 
 	// Use this for initialization
 	void Start () {
@@ -27,13 +40,13 @@ public class Turret : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         //Modify local rotation so that .rotation returns rotation relative to world
-
+        if (Input.GetMouseButtonDown(0)) Fire();
 
         //RayCast to terrain
         RaycastHit hit = new RaycastHit();
         int mask = 1 << 8;
         Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-        Physics.Raycast(ray, out hit, Mathf.Infinity, mask);
+        Physics.Raycast(ray, out hit, Mathf.Infinity);
         //Physics.Raycast(cam.transform.position, cam.transform.rotation.eulerAngles, out hit, Mathf.Infinity, mask);
         if (hit.collider != null) {
             switch (hit.collider.gameObject.layer) {
@@ -41,9 +54,10 @@ public class Turret : MonoBehaviour {
                 //case gui:
                     
                     break;
-                case 1:
-                    Quaternion q = Quaternion.LookRotation(hit.collider.gameObject.transform.position - barrel.transform.position);
-                    barrel.transform.rotation = q;
+                case 8:
+                    Quaternion q = Quaternion.LookRotation(hit.collider.gameObject.transform.position - barrel.transform.position, Vector3.up);
+                    barrelRotationVert = Mathf.Clamp(-90f - q.eulerAngles.x,-105f,-61f);
+                    barrelRotationHor = 180f + q.eulerAngles.y;
                     //Vector3.RotateTowards(barrel.transform.rotation.eulerAngles, hit.collider.gameObject.transform.position - barrel.transform.position);
                     break;
                 default:
@@ -55,8 +69,6 @@ public class Turret : MonoBehaviour {
     public void Fire() {
         GameObject obj = Instantiate(shellPrefab, barrelEnd.transform.position, Quaternion.LookRotation(barrelEnd.transform.position-barrel.transform.position));
         Shell shell = obj.GetComponent<Shell>();
-        for (int i = 0; i < modList.GetLength(1); ++i) {
-            shell.AddModifer(modList[currentAmmo][i]);
-        }
+        shell.GetComponent<Rigidbody>().velocity = transform.rotation * Vector3.forward * 1f;
     }
 }
